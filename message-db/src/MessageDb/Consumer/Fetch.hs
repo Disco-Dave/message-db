23b81270beeb6cd7045ba7da5cc2bb593fd320ab
disco-dave <1124825-Disco-Dave@users.noreply.gitlab.com>
2022-11-26 19:33:35 -0500

    added a fetch function

--- message-db/src/MessageDb/Consumer/Fetch.hs
+++ message-db/src/MessageDb/Consumer/Fetch.hs
@@ -1,11 +1,31 @@
-module MessageDb.Consumer.Fetch where
+module MessageDb.Consumer.Fetch
+  ( FetchSnapshot (..)
+  , FetchOptions (..)
+  , fetch
+  )
+where
 
+import Control.Monad (join)
 import Control.Monad.Except (MonadIO)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Foldable (for_)
+import Data.List.NonEmpty (nonEmpty)
+import Data.Maybe (fromMaybe)
 import Data.Pool (Pool)
+import qualified Data.Pool as Pool
 import qualified Database.PostgreSQL.Simple as Postgres
 import MessageDb.Consumer.BatchSize (BatchSize)
-import MessageDb.Consumer.Projection (Projected, Projection)
+import qualified MessageDb.Consumer.BatchSize as BatchSize
+import MessageDb.Consumer.Condition (Condition)
+import MessageDb.Consumer.Projection
+  ( Projected (..)
+  , Projection (..)
+  , emptyProjection
+  , project
+  )
+import qualified MessageDb.Functions as Functions
 import MessageDb.Message.StreamName (StreamName)
+import qualified MessageDb.StreamVersion as StreamVersion
 
 
 data FetchSnapshot m state = FetchSnapshot
@@ -16,12 +36,64 @@ data FetchSnapshot m state = FetchSnapshot
 
 data FetchOptions m state = FetchOptions
   { fetchPool :: Pool Postgres.Connection
-  , fetchBatchSize :: BatchSize
+  , fetchBatchSize :: Maybe BatchSize
   , fetchStreamName :: StreamName
   , fetchProjection :: Projection state
   , fetchSnapshot :: Maybe (FetchSnapshot m state)
+  , fetchCondition :: Maybe Condition
   }
 
 
 fetch :: MonadIO m => FetchOptions m state -> m (Maybe (Projected state))
-fetch = undefined
+fetch FetchOptions{..} = do
+  snapshot <- join <$> traverse retrieveSnapshot fetchSnapshot
+
+  let initialProjected =
+        fromMaybe (emptyProjection (initialState fetchProjection)) snapshot
+
+      loop !currentProjected = do
+        let currentStreamVersion =
+              projectedStreamVersion currentProjected
+
+            queryPosition =
+              case currentStreamVersion of
+                StreamVersion.DoesNotExist -> Nothing
+                StreamVersion.DoesExist position -> Just $ position + 1
+
+            currentProjection =
+              fetchProjection
+                { initialState = projectedState currentProjected
+                }
+
+        maybeMessages <-
+          liftIO . fmap nonEmpty . Pool.withResource fetchPool $ \connection ->
+            Functions.getStreamMessages
+              connection
+              fetchStreamName
+              queryPosition
+              fetchBatchSize
+              fetchCondition
+
+        case (maybeMessages, currentStreamVersion) of
+          (Nothing, StreamVersion.DoesNotExist) ->
+            pure Nothing
+          (Nothing, StreamVersion.DoesExist _) ->
+            pure $ Just currentProjected
+          (Just messages, _) -> do
+            let nextProjected =
+                  let newProjected = project currentProjection messages
+                   in newProjected
+                        { projectedErrors =
+                            projectedErrors newProjected <> projectedErrors currentProjected
+                        }
+
+            for_ fetchSnapshot $ \FetchSnapshot{recordSnapshot} ->
+              recordSnapshot nextProjected
+
+            case fetchBatchSize of
+              Just BatchSize.Unlimited ->
+                pure $ Just nextProjected
+              _ ->
+                loop nextProjected
+
+  loop initialProjected
